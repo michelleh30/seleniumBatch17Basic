32fb307a7079aaaaaab291a1e62456c0a49842be
michelleh30 <mdhurtad15@gmail.com>
2023-09-26 21:58:55 -0400

    class3

---
+++ src/test/java/class3/Xpath.java
@@ -0,0 +1,70 @@
+package class3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();//
+
+        //use the driver.get() method
+        driver.get("http://35.175.58.98/Xpath.php");
+
+        driver.manage().window().maximize();
+
+        //find the test box and send some keys
+        WebElement textBox1= driver.findElement(By.xpath("//input[@id='title']"));
+        textBox1.sendKeys("batch17");
+
+        //find the next text box and send keys
+        WebElement textBox2=driver.findElement(By.xpath("//input[@name='title']"));
+        textBox2.sendKeys("batch17");
+
+        //send the security question
+        WebElement question1= driver.findElement(By.xpath("//input[contains(@name,'security')]"));
+        question1.sendKeys("whats the color of the sun?");
+
+        //get the text
+        WebElement themagicalText= driver.findElement(By.xpath("//label[contains(text(),'Lorem')]"));
+        System.out.println(themagicalText.getText());
+
+        //get the text box
+        WebElement textBoxAPI= driver.findElement(By.xpath("//input[starts-with(@id,'api')]"));
+        textBoxAPI.sendKeys("abracadabra");
+
+        //get the email
+        WebElement Email1= driver.findElement(By.xpath("(//input[@class='form-control backup'])[1]"));
+        Email1.sendKeys("hello1@gmail.com");
+
+        //get the email2
+        WebElement Email2= driver.findElement(By.xpath("(//input[@class='form-control backup'])[2]"));
+        Email2.sendKeys("canicomein@gmail.com");
+
+        //get the email3
+        WebElement Email3= driver.findElement(By.xpath("(//input[@class='form-control backup'])[3]"));
+        Email3.sendKeys("welcome@gmail.com");
+
+        //get the below fields --- we can use to types to make it unique
+        WebElement Field1= driver.findElement(By.xpath("//input[@data-detail='one' and @name='customField']"));
+        Field1.sendKeys("cat");
+
+        //get 2nd fields
+        WebElement Field2= driver.findElement(By.xpath("//input[@name='customField' and @data-detail='two']"));
+        Field2.sendKeys("caterpillar");
+
+        //
+        WebElement choiceField1= driver.findElement(By.xpath("//input[@name='customField1' and @data-detail='one']"));
+        choiceField1.sendKeys("apple");
+
+        WebElement choiceField2= driver.findElement(By.xpath("//input[@name='customField1' and @data-detail='two']"));
+        choiceField2.sendKeys("happy");
+
+
+
+
+
+
+    }
+}
